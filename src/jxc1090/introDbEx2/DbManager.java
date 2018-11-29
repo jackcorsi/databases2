0b6817bf39c4cc515ea196028ac6207cd496f02c
jxc1090 <jxc1090@student.bham.ac.uk>
2018-11-29 19:29:12 +0000

    Connected

--- src/jxc1090/introDbEx2/DbManager.java
+++ src/jxc1090/introDbEx2/DbManager.java
@@ -1,9 +1,15 @@
 package jxc1090.introDbEx2;
 
-import java.sql.*;
-import javax.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DbManager {
+
+    private static final String DB_NAME = "jdbc:postgresql://mod-intro-databases.cs.bham.ac.uk/jxc1090";
+    private static final String USERNAME = "jxc1090";
+    private static final String PASSWORD = "a0gsd8vrru";
+    private static Connection conn;
 
     public static void main(String[] args) {
         try {
@@ -12,6 +18,21 @@ public class DbManager {
             System.out.println("Driver not found!");
             return;
         }
-        System.out.println("Yay it worked!");
+
+        try {
+            conn = DriverManager.getConnection(DB_NAME, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("SQLException:");
+            System.out.println(e.getCause());
+            return;
+        }
+
+        if (conn == null) {
+            System.out.println("Connection failed");
+            return;
+        }
+
+        System.out.println("Connection successful!");
+        
     }
 }
