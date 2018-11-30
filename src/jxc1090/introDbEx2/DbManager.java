8be9dcdeb4d7df5fc00d2b9179fb482f8ba76b37
jxc1090 <jxc1090@student.bham.ac.uk>
2018-11-30 15:57:56 +0000

    I have sql code now

--- src/jxc1090/introDbEx2/DbManager.java
+++ src/jxc1090/introDbEx2/DbManager.java
@@ -1,7 +1,9 @@
 package jxc1090.introDbEx2;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,39 +18,36 @@ public class DbManager {
     private static final String PASSWORD = "a0gsd8vrru";
     private static Connection conn;
     
-    private static final Path res_DbInit = Paths.get(DbManager.class.getResource("ex2/res/jxc1090/introDbEx2/DbManager/DbInit.sql").getPath());
+    private static BufferedReader src_DbInit = new BufferedReader(new InputStreamReader(DbManager.class.getResourceAsStream("/DbInit.sql")));
 
-    public static void main(String[] args) {
-    	String query;
-    	try {
-			query = new String(Files.readAllBytes(res_DbInit));
-		} catch (IOException e1) {
-			System.err.println("MISSING RESOURCE FILE LMAO");
-		}
+    public static void main(String[] args) throws IOException {
+    	String line;
+    	while ( (line = src_DbInit.readLine()) != null ) 
+    		System.out.println(line);
     	
     	
     	
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Driver not found!");
-            return;
-        }
-
-        try {
-            conn = DriverManager.getConnection(DB_NAME, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            System.out.println("SQLException:");
-            System.out.println(e.getCause());
-            return;
-        }
-
-        if (conn == null) {
-            System.out.println("Connection failed");
-            return;
-        }
-
-        System.out.println("Connection successful!");
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//        } catch (ClassNotFoundException e) {
+//            System.out.println("Driver not found!");
+//            return;
+//        }
+//
+//        try {
+//            conn = DriverManager.getConnection(DB_NAME, USERNAME, PASSWORD);
+//        } catch (SQLException e) {
+//            System.out.println("SQLException:");
+//            System.out.println(e.getCause());
+//            return;
+//        }
+//
+//        if (conn == null) {
+//            System.out.println("Connection failed");
+//            return;
+//        }
+//
+//        System.out.println("Connection successful!");
         
     }
 }
